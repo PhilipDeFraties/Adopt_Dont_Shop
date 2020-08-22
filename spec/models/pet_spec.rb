9d8214d27b82c8fb4ff65da3c868d8a1e1d48ccb
Philip DeFraties <philipjfry338@gmail.coom>
2020-08-22 14:08:44 -0600

    Add relationship test to pet_spec.

--- spec/models/pet_spec.rb
+++ spec/models/pet_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 describe Pet, type: :model do
+  describe 'relationships' do
+    it {should belong_to :shelter}
+  end
+
   describe "validations" do
     it { should validate_presence_of :image_path }
     it { should validate_presence_of :name }
